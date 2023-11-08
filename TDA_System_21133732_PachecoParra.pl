bfbee65bc0956a17e9b9a845c87b812fb6126205
Reinaldo Pacheco <reinaldo.pacheco@usach.cl>
2023-11-08 01:24:31 -0300

    add selector-predicates in TDA_System
    se agregan los predicados selectores en el TDA_System

--- TDA_System_21133732_PachecoParra.pl
+++ TDA_System_21133732_PachecoParra.pl
@@ -10,6 +10,38 @@
 % MetaSecundaria: removeDuplicates/4
 system(SystemName, InitialChatbotCodeLink, Chatbots, [SystemName, InitialChatbotCodeLink, Chatbots, [], [], []]).
 
-% pruebas
-% system("NewSystem", 0, [ ], S0).
-% system("NewSystem", 1, [CB11], S1).
+% nombre predicado: getSystemName
+% Descripción: predicado que devuelve el nombre de un sistema
+% Dom: system X Systemname
+% MetaPrimaria: getSystemName/2
+getSystemName([SystemName|_], SystemName).
+
+% nombre predicado: getSystemInitialChatbotCodeLink
+% Descripción: predicado que devuelve el link del chatbot inicial de un sistema
+% Dom: system X InitialChatbotCodeLink
+% MetaPrimaria: getSystemInitialChatbotCodeLink/2
+getSystemInitialChatbotCodeLink([_, InitialChatbotCodeLink|_], InitialChatbotCodeLink).
+
+% nombre predicado: getSystemChatbots
+% Descripción: predicado que devuelve los chatbots de un sistema
+% Dom: system X Chatbots
+% MetaPrimaria: getSystemChatbots/2
+getSystemChatbots([_,_,Chatbots|_], Chatbots).
+
+% nombre predicado: getSystemChatHistory
+% Descripción: predicado que devuelve el historial de un sistema
+% Dom: system X ChatHistory
+% MetaPrimaria: getSystemChatHistory/2
+getSystemChatHistory([_, _, _, _, ChatHistory, _], ChatHistory).
+
+% nombre predicado: getUsuarioRegistrado
+% Descripción: predicado que devuelve los usuarios registrados en el sistema
+% Dom: system X Users
+% MetaPrimaria: getUsuarioRegistrado/2
+getUsuarioRegistrado([_, _, _, Users, _, _], Users).
+
+% nombre funcion: getUsuarioLogeado
+% Descripción: predicado que devuelve el usuario logueado en el sistema
+% Dom: system X userLogin
+% MetaPrimaria: getUsuarioLogeado/2
+getUsuarioLogeado([_, _, _, _, UserLogin, _], UserLogin).
